c685fa8fa7a4ed3c72d5801d376e92c6471269eb
MyMac <mymac@MyMacs-MacBook-Pro.local>
2025-05-09 23:49:12 -0700

    Remove temporary test/[id] route

--- src/app/test:[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-// src/app/test/[id]/page.tsx
-export default function TestIdPage({ params }: { params: { id: string } }) {
-  return (
-    <div>
-      <h1>Test Page</h1>
-      <p>ID: {params.id}</p>
-    </div>
-  );
-}
