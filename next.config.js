ac86e5a513cb93af2ffa293e9014e870095c1443
MyMac <mymac@MyMacs-MacBook-Pro.local>
2025-05-09 23:13:24 -0700

    Convert next.config.ts to next.config.js and update content

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  // If you had any specific configurations, make sure they are valid JavaScript.
+  // For example, if you had reactStrictMode: true, it would look like:
+  // reactStrictMode: true,
+};
+
+module.exports = nextConfig;
