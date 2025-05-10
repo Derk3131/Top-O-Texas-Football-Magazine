3601c49e4598d6c12c18f76b1585733391f5c97b
MyMac <mymac@MyMacs-MacBook-Pro.local>
2025-05-09 23:44:31 -0700

    Restore full functionality to articles/[slug] page

--- src/app/articles/[slug]/_page.tsx
+++
@@ -1,9 +0,0 @@
-// src/app/articles/[slug]/page.tsx
-export default function ArticlePage({ params }: { params: { slug: string } }) {
-  return (
-    <div>
-      <h1>Article Slug: {params.slug}</h1>
-      <p>This is a simplified page for testing the dynamic route parameter.</p>
-    </div>
-  );
-}
